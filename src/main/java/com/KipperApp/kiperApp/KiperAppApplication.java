d6692c19e0fb19fdb2e15e9bcce9fbdbf8255ce5
joao victor <abutodzn@gmail.com>
2025-03-17 15:44:04 -0300

    first commit

---
+++ src/main/java/com/KipperApp/kiperApp/KiperAppApplication.java
@@ -0,0 +1,13 @@
+package com.KipperApp.kiperApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiperAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiperAppApplication.class, args);
+	}
+
+}
